ae10c5e75e42b49fcfcc136bc67db65af8d93648
henok <mitkuhenok@gmail.com>
2023-01-10 11:00:30 +0300

    adding pages and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgetPassword from "./pages/ForgetPassword";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+
 function App() {
-  return <h1 className="text-8xl">henok mitku</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
